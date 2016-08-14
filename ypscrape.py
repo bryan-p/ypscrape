77189059e7c1dc14f5fc36f737e2f3f2d9f9d9f0
bryan <bryan@localhost.localdomain>
2016-08-13 22:05:00 -0400

    only add companies with selected zip code. move main body in loop

--- ypscrape.py
+++ ypscrape.py
@@ -1,27 +1,29 @@
 import os, requests, bs4, re, csv, datetime
 
 fqdn = "http://www.yellowpages.com"
+filename = ""
 category = ""
 zipcode = ""
-
+pages = 0
+companies = {}
 
 def get_company_info(results):
-    companies = {}
-    index = 0
+    global companies
     for element in results:
-        index += 1
-        result = element.find('div', class_='info')
-
-        name = result.find('a', class_='business-name').get_text().encode('utf-8')
-        phone = result.find('div', class_='phones').get_text()
-
-        companies[name] = {}
-        companies[name]['phone'] = phone.encode('utf-8')
-
+        name = ""
+        phone = ""
         street = ""
         city = ""
         state = ""
         zip = ""
+
+        result = element.find('div', class_='info')
+
+        name = result.find('a', class_='business-name').get_text().encode('utf-8')
+        phoneDiv = result.find('div', class_='phones')
+        if (phoneDiv is not None):
+            phone = phoneDiv.get_text()
+
         addr = result.find_all('p', class_='adr')
         for tag in addr:
             if (tag.span is None):
@@ -37,42 +39,60 @@ def get_company_info(results):
         
             if(span2 is not None):
                 city = span2.get_text()
+                city = city[:-2]
             if(span3 is not None):
                 state = span3.get_text()
             if(span4 is not None):
                 zip = span4.get_text()
 
+        if (zip == zipcode):
+            companies[name] = {}
+            companies[name]['phone'] = phone.encode('utf-8')
             companies[name]['street'] = street.encode('utf-8')
             companies[name]['city'] = city.encode('utf-8')
             companies[name]['state'] = state.encode('utf-8')
             companies[name]['zip'] = zip.encode('utf-8')
 
-    return companies
-
 def get_input():
     global category     
     global zipcode
+    global pages
 
     category = raw_input("Search Term: ").strip()
     zipcode = raw_input("Zip Code: ").strip()
+    pages = raw_input("max number pages to search [ 0 for no max]: ").strip()
+    pages = int(pages)
     urlquery = "/search?search_terms=" + category + "&geo_location_terms=" + zipcode 
 
     print("Scraping data for '{}' in zip code '{}'...").format(category, zipcode)
 
     return urlquery
 
-def save_csv(companies):
-    with open(category + "_" + zipcode + " " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M%s"), "w") as toWrite:
+def save():
+    global filename 
+    filename = category + "_" + zipcode + " " + datetime.datetime.now().strftime("%Y-%m-%dT%H%M%S" + ".csv")
+    with open(filename, "w") as toWrite:
         writer = csv.writer(toWrite, delimiter=",")
         writer.writerow(["name", "phone", "street", "city", "state", "zip"])
         for key in companies.keys():
             writer.writerow([key, companies[key]["phone"], companies[key]["street"], companies[key]["city"], companies[key]["state"], companies[key]["zip"]])
 
-
 query = get_input()
-response = requests.get(fqdn+query)
-soup = bs4.BeautifulSoup(response.text, 'html.parser')
-search_results = soup.find_all('div', id=re.compile('lid-*'))
 
-companies = get_company_info(search_results)
-save_csv(companies)
+while True:
+    response = requests.get(fqdn+query)
+    soup = bs4.BeautifulSoup(response.text, 'html.parser')
+    
+    nextPage = soup.find('a', class_='next')
+    search_results = soup.find_all('div', id=re.compile('lid-*'))
+    get_company_info(search_results)
+
+    if(nextPage is None):
+       print("No more pages")
+       break
+    else:
+       print("searching next page: " + nextPage.get('href'))
+       query = nextPage.get("href") 
+         
+print("Saving data to file: {} in folder: {}").format(filename, os.getcwd())
+save()
